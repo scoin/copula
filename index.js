253ee206d5e53bde973cc40c9da3fc95eea2ad04
Greg Piccolo <greg.piccolo@gmail.com>
2016-10-08 00:24:33 -0400

    retain state

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 var path = require("path");
 
-module.exports = function(){
-    var context = {
-        //environment variables
-        env: {},
-        //environment mode, development, staging, production...
-        mode: "",
-        //globals setter and getter
-        globals: require(path.normalize(__dirname + "/lib/globals.js")),
-        //middleware
-        middleware: {},
-        //global file import function
-        modules: {},
-        //routes declaration
-        routes: {},
-        //validators
-        validators: {},
-        //public
-        publicDirectory: "",
-        //view engine
-        viewEngine: null,
-    }
-
-    //event callbacks
-    context.events = require(path.normalize(__dirname + "/lib/events.js")).call(context);
-
-    //service discovery interface
-    context.discovery = function(interface, config) { return require(path.normalize(__dirname + "/lib/discovery.js")).call(context, interface, config) }
-
-     //init function
-    context.init = function(protocol, hostname, port){ require(path.normalize(__dirname + "/lib/init.js")).call(context, protocol, hostname, port) }
-
-    return context;
+var context = {
+    //environment variables
+    env: {},
+    //environment mode, development, staging, production...
+    mode: "",
+    //globals setter and getter
+    globals: require(path.normalize(__dirname + "/lib/globals.js")),
+    //middleware
+    middleware: {},
+    //global file import function
+    module: require(path.normalize(__dirname + "/lib/modules.js")),
+    //routes declaration
+    routes: {},
+    //validators
+    validators: {},
+    //public
+    publicDirectory: "",
+    //view engine
+    viewEngine: null,
 }
+
+//event callbacks
+context.events = require(path.normalize(__dirname + "/lib/events.js")).call(context);
+
+//service discovery interface
+context.discovery = function(interface, config) { return require(path.normalize(__dirname + "/lib/discovery.js")).call(context, interface, config) }
+
+ //init function
+context.start = function(info){ require(path.normalize(__dirname + "/lib/init.js")).call(context, info) }
+
+module.exports = context;
